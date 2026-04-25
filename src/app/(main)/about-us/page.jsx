25f85c631b74ba55bac9b79ed654f3c82bc037a8
Johirul Islam Himel <himel1199@gmail.com>
2026-04-26 02:57:36 +0600

    first commit

---
+++ src/app/(main)/about-us/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const metadata = {
+  title: "Dragon News - about us",
+};
+
+const AboutUsPage = () => {
+  return <div>About us page</div>;
+};
+
+export default AboutUsPage;
---
+++ src/components/homepage/news/RightSidebar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+
+const RightSidebar = () => {
+  return (
+    <div className="space-y-6">
+      {/* Login Section  */}
+      <div>
+        <h2 className="font-bold text-lg mb-4">Login with</h2>
+        <div className="flex flex-col gap-2">
+          <button className="btn btn-outline border-blue-500 text-blue-500 w-full">
+            Login with Google
+          </button>
+          <button className="btn btn-outline border-black w-full">
+            Login with Github
+          </button>
+        </div>
+      </div>
+
+      {/* Find Us On Section  */}
+      <div>
+        <h2 className="font-bold text-lg mb-4">Find Us On</h2>
+        <div className="flex flex-col border rounded-lg overflow-hidden text-gray-600">
+          <a
+            href="https://facebook.com"
+            className="flex items-center gap-3 p-4 border-b hover:bg-gray-100 transition-all"
+          >
+            <span className="p-2 bg-gray-100 rounded-full text-blue-600">
+              <FaFacebook />
+            </span>
+            <span className="font-medium">Facebook</span>
+          </a>
+          <a
+            href="https://twitter.com"
+            className="flex items-center gap-3 p-4 border-b hover:bg-gray-100 transition-all"
+          >
+            <span className="p-2 bg-gray-100 rounded-full text-sky-400">
+              <FaTwitter />
+            </span>
+            <span className="font-medium">Twitter</span>
+          </a>
+          <a
+            href="https://instagram.com"
+            className="flex items-center gap-3 p-4 hover:bg-gray-100 transition-all"
+          >
+            <span className="p-2 bg-gray-100 rounded-full text-pink-600">
+              <FaInstagram />
+            </span>
+            <span className="font-medium">Instagram</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightSidebar;
